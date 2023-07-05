ccb615e6ed1792dc36820a1cff984c79600997ca
Hari om verma <117583499+hari7797@users.noreply.github.com>
2023-07-05 17:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-  
+   let radi=document.getElementById("radius").value;
+            let vol= document.getElementById("volume");
+       radi=Math.abs(radi);        
+	let cal=(4/3)*Math.PI*Math.pow(radi,3);
+    cal=cal.toFixed(4);            
+	vol.value=cal;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
